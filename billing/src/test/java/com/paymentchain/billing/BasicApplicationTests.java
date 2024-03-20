7b9f54e7ab20198bfca597aa18a81f60a4184298
Alejandro Rojas <rmae2727@gmail.com>
2024-03-20 11:20:17 -0600

    Add comment to test

--- billing/src/test/java/com/paymentchain/billing/BasicApplicationTests.java
+++ billing/src/test/java/com/paymentchain/billing/BasicApplicationTests.java
@@ -15,7 +15,6 @@ public class BasicApplicationTests {
 	//Hello world from MacOs
 	//Hello world :) 20-03-2024 
 
-	
  @Test
  public void contextLoads() {
  	    String meessage= "default message cambio test devops";
